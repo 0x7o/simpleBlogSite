69a9f6692b8dce6ebb3c6c76e415402fd3dd6387
0x7o <danil.kononyuk@ya.ru>
2024-05-30 11:10:34 +0800

    начальный коммит

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["158.160.93.71"]
+ALLOWED_HOSTS = ["localhost"]
 
 # Application definition
 
